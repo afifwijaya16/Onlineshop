67b6270bf5e02803f18995d310cd16f5c70754ea
afwjy16 <afif0071997@gmail.com>
2020-07-25 16:30:11 +0700

    update data new

---
+++ backend/data.js
@@ -0,0 +1,37 @@
+export default {
+  products: [
+    {
+      _id: "1",
+      name: "Slim Shirt",
+      category: "Shirts",
+      image: "/images/p1.jpg",
+      price: 60,
+      brand: " Nike",
+      rating: 4.5,
+      numReviews: 10,
+      description: "tes",
+    },
+    {
+      _id: "2",
+      name: "Slim T-Shirt",
+      category: "Shirts",
+      image: "/images/p2.jpg",
+      price: 60,
+      brand: " Nike",
+      rating: 4.5,
+      numReviews: 10,
+      description: "tes",
+    },
+    {
+      _id: "3",
+      name: "Slim T-Shirt Blue",
+      category: "Shirts",
+      image: "/images/p3.jpg",
+      price: 60,
+      brand: " Nike",
+      rating: 4.5,
+      numReviews: 10,
+      description: "tes",
+    },
+  ],
+};
