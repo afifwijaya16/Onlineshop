03c8112e9d0162e1f39ad2e561df7258c6fdfc8c
afwjy16 <afif0071997@gmail.com>
2020-07-28 14:47:22 +0700

    update js-cookie

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,19 +1,26 @@
 import { createStore, combineReducers, compose, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
+import Cookie from "js-cookie";
+import {
+  productListReducer,
+  productDetailReducer,
+} from "./reducers/productReducers";
 
-import { productListReducer, productDetailReducer } from "./reducers/productReducers";
 import { cartReducer } from "./reducers/cartReducers";
-const initialState = {};
+
+const cartItems = Cookie.getJSON("cartItems") || [];
+
+const initialState = { cart: { cartItems } };
 const reducer = combineReducers({
-    productList: productListReducer,
-    productDetail: productDetailReducer,
-    cart: cartReducer,
-})
+  productList: productListReducer,
+  productDetail: productDetailReducer,
+  cart: cartReducer,
+});
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
-    reducer,
-    initialState,
-    composeEnhancer(applyMiddleware(thunk))
+  reducer,
+  initialState,
+  composeEnhancer(applyMiddleware(thunk))
 );
 export default store;
